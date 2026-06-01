e8e43f35dd47d69f8ad0c1eb3c204cae0fd6ea9f
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-06-01 12:13:51 +0200

    filter funkar

--- cardshop/src/Pages/Menu.jsx
+++ cardshop/src/Pages/Menu.jsx
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom";
 import CardItem from "../Components/CardItem";
 
 function Menu() {
+  const [showFilter, setShowFilter] = useState(false);
+  const [selectedCondition, setSelectedCondition] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("");
   const [cards, setCards] = useState([]);
   // Här får menu category värdet av 1
   const { categoryId } = useParams();
@@ -22,11 +25,78 @@ function Menu() {
       });
   }, [categoryId]);
 
+  const filteredCards = cards.filter((card) => {
+    return (
+      (selectedCondition === "" || card.condition === selectedCondition) &&
+      // LANGUAGE
+
+      (selectedLanguage === "" || card.language === selectedLanguage)
+    );
+  });
+
   return (
-    <div className="menu-container">
-      {cards.map((card) => (
-        <CardItem key={card.id} card={card} />
-      ))}
+    <div className="Menu-page">
+      <button onClick={() => setShowFilter(true)} className="Filter-button">
+        Filter
+      </button>
+      {showFilter && (
+        <div className="Filter-sidebar">
+          <button onClick={() => setShowFilter(false)} className="Close-filter">
+            X
+          </button>
+
+          <h2>Filter</h2>
+
+          <h3>Condition</h3>
+
+          <button
+            onClick={() => setSelectedCondition("mint")}
+            className="filter-button"
+          >
+            Mint
+          </button>
+          <button
+            onClick={() => setSelectedCondition("near mint")}
+            className="filter-button"
+          >
+            near mint
+          </button>
+          <button
+            onClick={() => setSelectedCondition("")}
+            className="filter-button"
+          >
+            Clear Condition
+          </button>
+
+          <h3>Language</h3>
+
+          <button
+            onClick={() => setSelectedLanguage("english")}
+            className="filter-button"
+          >
+            English
+          </button>
+
+          <button
+            onClick={() => setSelectedLanguage("japanese")}
+            className="filter-button"
+          >
+            Japanese
+          </button>
+
+          <button
+            onClick={() => setSelectedLanguage("")}
+            className="filter-button"
+          >
+            Clear Language
+          </button>
+        </div>
+      )}
+      <div className="menu-container">
+        {filteredCards.map((card) => (
+          <CardItem key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
