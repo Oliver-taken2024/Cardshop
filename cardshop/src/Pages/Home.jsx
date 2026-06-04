39cb090c28a0e5d59d6047608137af38f328b33b
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-06-04 14:30:29 +0200

    Klar

--- cardshop/src/Pages/Home.jsx
+++ cardshop/src/Pages/Home.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../Components/Hero";
+import CardItem from "../Components/CardItem";
 
 function Home() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cards")
+      .then((res) => res.json())
+      .then((data) => {
+        // ta några "populära"
+
+        const shuffled = data.sort(() => 0.5 - Math.random());
+        setCards(shuffled.slice(0, 6));
+        // första 6 korten
+        setCards(popular);
+      });
+  }, []);
+
   return (
     <div className="Home-page">
-      <div>
-        <Hero />
+      <Hero />
+
+      <h2 className="home-title">Popular Cards</h2>
+
+      <div className="home-cards">
+        {cards.map((card) => (
+          <CardItem key={card.id} card={card} />
+        ))}
       </div>
     </div>
   );
